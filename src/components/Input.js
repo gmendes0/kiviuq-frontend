4ca547cb5adc2a0c1b8aa22f4912fcac5945fbd5
mendesg0 <gabriel@prestige.com.br>
2020-02-04 13:49:47 -0300

    feat(register): sending data to api

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,18 @@
 import React from "react";
 
-export default function Input({ field, type, label }) {
+export default function Input({ field, type, label, onChange }) {
   return (
     <div className="form-group row">
-      <label htmlFor={field} className="col-form-label col-sm-2">
+      <label htmlFor={field} className="col-form-label col-md-3 text-sm-right">
         {label}
       </label>
-      <div className="col-sm-10">
-        <input className="form-control" type={type} id={field} />
+      <div className="col-md-7">
+        <input
+          className="form-control"
+          type={type}
+          id={field}
+          onChange={onChange}
+        />
       </div>
     </div>
   );
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import api from "../services/api";
 
 import Input from "../components/Input";
 
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmation, setConfirmation] = useState("");
+
+  async function handleSubmit() {
+    const response = await api
+      .post(
+        "/register",
+        {
+          username,
+          email,
+          password
+        },
+        {
+          headers: {
+            Accept: "application/json"
+          }
+        }
+      )
+      .catch(error => console.log(error.response.data));
+
+    /**
+     * caso haja erro
+     */
+    if (!response) return null;
+
+    /**
+     * resposta sem erros
+     */
+    console.log(response.data);
+  }
+
   return (
     <div className="container">
       <div className="text-center">
@@ -10,7 +44,40 @@ export default function Register() {
           <div className="card col-md-8 mt-5 py-5">
             <h1 className="text-muted mb-5">Registrar</h1>
             <div className="card-body">
-              <Input type="text" field="username" label="username" />
+              <Input
+                type="text"
+                field="username"
+                label="username *"
+                value={username}
+                onChange={event => setUsername(event.target.value)}
+              />
+              <Input
+                type="email"
+                field="email"
+                label="email *"
+                value={email}
+                onChange={event => setEmail(event.target.value)}
+              />
+              <Input
+                type="password"
+                field="password"
+                label="senha *"
+                value={password}
+                onChange={event => setPassword(event.target.value)}
+              />
+              <Input
+                type="password"
+                field="password"
+                label="confirme a senha *"
+                value={confirmation}
+                onChange={event => setConfirmation(event.target.value)}
+              />
+              <button
+                className="text-center btn btn-success mt-5"
+                onClick={handleSubmit}
+              >
+                Registrar
+              </button>
             </div>
           </div>
         </div>
